14f351e6779b7d22fb02fb1bd8d9e2644a13f53a
Nidvar <djayshine87@hotmail.com>
2025-07-18 11:45:47 +1000

    moved app.ts logic to api folder

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,34 @@
-import { createApp } from '../src/app';
-import { Request, Response } from 'express';
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { connectDB } from '../config/db.js';
+import { routes } from '../routes/main.js';
 
-let cachedApp: any;
+dotenv.config();
+connectDB();
 
-export default async function handler(req: Request, res: Response){
-    if(!cachedApp){
-        cachedApp = await createApp();
-    };
-    cachedApp(req, res);
-}
+const app = express();
+
+const allowedOrigins = ['https://cybermern.vercel.app', 'http://localhost:5173'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // if you're using cookies/auth headers
+}));
+
+app.use(cookieParser());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
+routes(app);
+
+export default app;
