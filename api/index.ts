41795540eae1a8e09d829cf0355f91f5858a64e7
Nidvar <djayshine87@hotmail.com>
2025-07-18 11:58:58 +1000

    backend is alive

--- api/index.ts
+++ api/index.ts
@@ -31,6 +31,10 @@ app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Backend is alive!');
+});
+
 app.use('/', mainRouter);
 app.use('/notes', notesRoutes);
 
